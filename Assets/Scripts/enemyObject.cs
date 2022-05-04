8878e2573660e9f4913f6d02b89c42f9048c446a
Harel Don-Yehiya <harel.don@cs.huji.ac.il>
2022-05-04 12:58:36 +0300

    update enemy

--- Assets/Scripts/enemyObject.cs
+++ Assets/Scripts/enemyObject.cs
@@ -24,4 +24,6 @@ public class enemyObject : MonoBehaviour
         }
         return false;
     }
+    
+    
 }
--- Assets/Scripts/simpleEnemy.cs
+++ Assets/Scripts/simpleEnemy.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,6 +10,10 @@ public class simpleEnemy : enemyObject
     [SerializeField] public GameObject player;
 
     [SerializeField] private float speed = 2f;
+    [SerializeField] private float len = 4f;
+    [SerializeField] private Transform[] places;
+
+    [SerializeField] private int counter;
 
     // Start is called before the first frame update
     void Start()
@@ -19,20 +24,29 @@ public class simpleEnemy : enemyObject
     {
         movement = gameObject.transform.position;
         rb = GetComponent<Rigidbody2D>();
+        counter = 0;
     }
 
     // Update is called once per frame
     private void Update()
-    { 
+    {
         _currentTarget = player.transform.position;
     }
 
+    // physics is best, when activating it in Fixed update. 
     private void FixedUpdate()
     {
         if (!isAlive()) gameObject.SetActive(false);
-        move();
+        if (Math.Abs(PositionX()) <= len)
+        {
+            move();
+        }
+        else
+        {
+            moveByTarget();
+        }
     }
- 
+
     /**
      * move the enemy by following the player.
      */
@@ -40,6 +54,18 @@ public class simpleEnemy : enemyObject
     {
         rb.velocity = new Vector2((PositionX() < 0 ? speed : -speed),
             rb.velocity.y);
+    }
+
+    /**
+     * move the enemy by following the places transform positions.
+     */
+    void moveByTarget()
+    {
+        var x = transform.position.x - places[counter].position.x;
+        rb.velocity = new Vector2((x <= 0 ? speed : -speed),
+            rb.velocity.y);
+        if (Math.Abs(x) > 0.1f) return;
+        counter = (counter >= places.Length - 1) ? 0 : counter + 1;
     }
 
     private float PositionX()
