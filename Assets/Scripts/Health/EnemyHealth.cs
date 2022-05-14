f2ba41f89d38a8987a80e175e0bd1f227f745112
GalShaffir <80719879+GalShap@users.noreply.github.com>
2022-05-14 12:09:09 +0300

    added flashing enemy

--- Assets/Scripts/Health/EnemyHealth.cs
+++ Assets/Scripts/Health/EnemyHealth.cs
@@ -1,16 +1,70 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class EnemyHealth : MonoBehaviour, IDamageable
 {
-    [SerializeField] private int health = 100;
+    #region Inspector
 
+    [SerializeField] private int health = 100;
+    
+    [SerializeField] private SpriteRenderer enemySpriteRenderer;
+
+    [Tooltip("How much time the animation takes for every blip?")] 
+    [SerializeField] private float timeToAnimate = 0.1f;
+    
+    #endregion
+
+    #region Constants
+    
     private const int MAX_HEALTH = 100;
 
     private const int MIN_HEALTH = 0;
 
+    private const int MAX_ALPHA = 1;
+
+    private const int MIN_ALPHA = 0;
+    
+    #endregion
+
+    private IEnumerator DamageFlashAnimation(int count)
+    {   
+        
+        float elapsedTimeMax = 0;
+        float elapsedTimeMin = 0;
+        Color alpha = enemySpriteRenderer.color;
+        for (int i = 0; i < count; i++)
+        {
+            elapsedTimeMax = 0;
+            elapsedTimeMin = 0;
+            
+            while (elapsedTimeMin < timeToAnimate)
+            {   
+      
+                alpha.a =  Mathf.Lerp(MAX_ALPHA, MIN_ALPHA, (elapsedTimeMin / timeToAnimate));
+                enemySpriteRenderer.color = alpha;
+                elapsedTimeMin += Time.deltaTime;
+                yield return null;
+            }
+
+            alpha.a = MIN_ALPHA;
+            
+            while (elapsedTimeMax < timeToAnimate)
+            {   
+         
+                alpha.a =  Mathf.Lerp(MIN_ALPHA, MAX_ALPHA, (elapsedTimeMax / timeToAnimate));
+                enemySpriteRenderer.color = alpha;
+                elapsedTimeMax += Time.deltaTime;
+                yield return null;
+            }
+
+            alpha.a = MAX_ALPHA;
+        }
+        
+        
+    }
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("AttackCollider"))
@@ -22,6 +76,7 @@ public class EnemyHealth : MonoBehaviour, IDamageable
 
     public void Damage(int amount)
     {
+        StartCoroutine(DamageFlashAnimation(1));
         health -= amount;
         if (health < MIN_HEALTH)
             health = MIN_HEALTH;
