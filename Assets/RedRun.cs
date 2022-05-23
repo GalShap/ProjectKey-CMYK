886df8eccc8b9e4d290b702eca1d45e723449f44
Harel Don-Yehiya <harel.don@cs.huji.ac.il>
2022-05-23 18:05:29 +0300

    update

--- Assets/RedRun.cs
+++ Assets/RedRun.cs
@@ -6,24 +6,37 @@ public class RedRun : StateMachineBehaviour
 {
     private GameObject player;
     private Rigidbody2D rb;
+
+    private MagentaGod red;
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         player = GameObject.FindGameObjectWithTag("Player");
+        red = animator.GetComponent<MagentaGod>();
         rb = animator.GetComponent<Rigidbody2D>();
+        
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
-    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        red.LookAtPlayer();
+
+        Vector2 target = new Vector2(player.transform.position.x, rb.transform.position.y);
+        Vector2 newPos = Vector2.MoveTowards(rb.position, target, red.speed * Time.fixedDeltaTime);
+        rb.MovePosition(newPos);
+
+        if (Vector2.Distance(player.transform.position, rb.position) <= red.attackRange)
+        {
+            animator.SetTrigger("Attack");
+        }
+    }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.ResetTrigger("Attack");
+    }
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
     //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
--- Assets/Scripts/enemy/MagentaGod.cs
+++ Assets/Scripts/enemy/MagentaGod.cs
@@ -6,10 +6,13 @@ using UnityEngine;
 public class MagentaGod : EnemyObject
 {
     [SerializeField] private float timerCounter = 3;
+    [SerializeField] public float speed = 3;
+    [SerializeField] public float attackRange = 3;
     private float timer = 0;
     public GameObject m_Projectile; // this is a reference to your projectile prefab
     public Transform m_SpawnTransform; // this is a reference to the transform where the prefab will spawn
     public GameObject player;
+    
     // Start is called before the first frame update
     private void Awake()
     {
@@ -20,14 +23,14 @@ public class MagentaGod : EnemyObject
 
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        timer -= Time.deltaTime;
-        if (!(timer <= 0)) return;
-        Shoot();
-        timer = timerCounter;
-    }
+    // // Update is called once per frame
+    // void Update()
+    // {
+    //     timer -= Time.deltaTime;
+    //     if (!(timer <= 0)) return;
+    //     Shoot();
+    //     timer = timerCounter;
+    // }
 
     private void Shoot()
     { 
@@ -37,5 +40,26 @@ public class MagentaGod : EnemyObject
     protected override void UponDead()
     {
         gameObject.SetActive(false);
+    }
+    
+    public bool isFlipped = false;
+
+    public void LookAtPlayer()
+    {
+        Vector3 flipped = transform.localScale;
+        flipped.z *= -1f;
+
+        if (transform.position.x > player.transform.position.x && isFlipped)
+        {
+            transform.localScale = flipped;
+            transform.Rotate(0f, 180f, 0f);
+            isFlipped = false;
+        }
+        else if (transform.position.x < player.transform.position.x && !isFlipped)
+        {
+            transform.localScale = flipped;
+            transform.Rotate(0f, 180f, 0f);
+            isFlipped = true;
+        }
     }
 }
