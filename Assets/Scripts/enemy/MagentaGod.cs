2ba8c834411adb8e11356188ea794706e9c2edb6
Harel Don-Yehiya <harel.don@cs.huji.ac.il>
2022-05-26 13:48:33 +0300

    update

--- Assets/Scripts/enemy/MagentaGod.cs
+++ Assets/Scripts/enemy/MagentaGod.cs
@@ -2,14 +2,18 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class MagentaGod : EnemyObject
 {
     [SerializeField] private float timerCounter = 3;
+    [SerializeField] public float speed = 3;
+    [SerializeField] public float attackRange = 3;
     private float timer = 0;
     public GameObject m_Projectile; // this is a reference to your projectile prefab
-    public Transform m_SpawnTransform; // this is a reference to the transform where the prefab will spawn
+    [SerializeField] private Transform [] m_SpawnTransform; // this is a reference to the transform where the prefab will spawn
     public GameObject player;
+    
     // Start is called before the first frame update
     private void Awake()
     {
@@ -19,23 +23,40 @@ public class MagentaGod : EnemyObject
         collisionOffset = Vector2.right * (_renderer.sprite.rect.width / _renderer.sprite.pixelsPerUnit) / 2;
 
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    public void Shoot()
     {
-        timer -= Time.deltaTime;
-        if (!(timer <= 0)) return;
-        Shoot();
-        timer = timerCounter;
-    }
-
-    private void Shoot()
-    { 
-        
+        int i = m_SpawnTransform.Length;
+        int k = Random.Range(0, i);
+        var dir = player.transform.position - transform.position;
+        var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        m_SpawnTransform[k].rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        Instantiate(m_Projectile, m_SpawnTransform[k].position, m_SpawnTransform[k].rotation);
     }
 
     protected override void UponDead()
     {
         gameObject.SetActive(false);
+    }
+    
+    public bool isFlipped = false;
+
+    public void LookAtPlayer()
+    {
+        Vector3 flipped = transform.localScale;
+        flipped.z *= -1f;
+        
+        if (transform.position.x > player.transform.position.x && isFlipped)
+        {
+            transform.localScale = flipped;
+            transform.Rotate(0f, 180f, 0f);
+            isFlipped = false;
+        }
+        else if (transform.position.x < player.transform.position.x && !isFlipped)
+        {
+            transform.localScale = flipped;
+            transform.Rotate(0f, 180f, 0f);
+            isFlipped = true;
+        }
     }
 }
---
+++ Assets/Scripts/enemy/RedRun.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedRun : StateMachineBehaviour
+{
+    private GameObject player;
+    private Rigidbody2D rb;
+
+    private MagentaGod red;
+    [SerializeField] private float timerCounter = 3;
+    private float timer = 0;
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        red = animator.GetComponent<MagentaGod>();
+        rb = animator.GetComponent<Rigidbody2D>();
+        
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        red.LookAtPlayer();
+
+        // Vector2 target = new Vector2(player.transform.position.x, rb.transform.position.y);
+        // Vector2 newPos = Vector2.MoveTowards(rb.position, target, red.speed * Time.fixedDeltaTime);
+        // rb.MovePosition(newPos);
+        timer += Time.deltaTime;
+        if (timer >= timerCounter)
+        {
+            timer = 0;
+            rb.AddForce(Vector2.up);
+            animator.SetTrigger("shoot");
+        }
+        // if (Vector2.Distance(player.transform.position, rb.position) <= red.attackRange)
+        // {
+            // animator.SetTrigger("shoot");
+        // }
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.ResetTrigger("shoot");
+    }
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
